518309b3d4848888bfa4494d6c2b48291ab0e946
sejin.kim1904@gmail.com <sejin.kim1904@gmail.com>
2020-10-24 14:02:00 -0600

    feat(auth_controller): Add auto_login action
    - Rename controller
    - Rename create action to login
    - Add skip_before_action callback

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,7 +1,8 @@
 module Api
   module V1
-    class SessionsController < ApplicationController
-      def create
+    class AuthController < ApplicationController
+      skip_before_action :require_login, only: [:login, :auto_login]
+      def login
         team = Team.find_by(email: params[:email])
 
         if team && team.authenticate(params[:password])
@@ -12,6 +13,16 @@ module Api
           render json: {
             error: 'Your email or password is incorrect.'
           }, status: :bad_request
+        end
+      end
+
+      def auto_login
+        if session_team
+          render json: TeamSerializer.new(session_team), status: :ok
+        else
+          render json: {
+            error: 'No user logged in'
+          }, status: :unauthorized
         end
       end
     end

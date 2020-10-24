cc6ef326cf99e13087c747d5df3365a63653663f
sejin.kim1904@gmail.com <sejin.kim1904@gmail.com>
2020-10-24 14:07:39 -0600

    test(auth): Create auto_login request spec
    - Renamed folder to match controller update
    - Update factory

--- spec/factories/team.rb
+++ spec/factories/team.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :team do
     email { Faker::Internet.email }
     password { '123456' }
+    password_confirmation { '123456' }
     team_name { Faker::Team.name }
   end
 end

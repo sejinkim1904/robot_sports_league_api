6433c05c8bb881e2fae86d2966eab5285a862e4b
sejin.kim1904@gmail.com <sejin.kim1904@gmail.com>
2020-10-24 15:57:06 -0600

    feat(teams): Add update endpoint
    - Update routes
    - Update message key to error
    - Create spec

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class TeamsController < ApplicationController
       skip_before_action :require_login, only: [:create]
-      
+
       def create
         team = Team.new(team_params)
 
@@ -12,6 +12,16 @@ module Api
           render json: {
             error: team.errors.full_messages.to_sentence
           }, status: :bad_request
+        end
+      end
+
+      def update
+        if @team.update(team_params)
+          render json: TeamSerializer.new(@team), status: :ok
+        else
+          render json: {
+            error: @team.errors.full_messages.to_sentence
+          }, status: :conflict
         end
       end
 

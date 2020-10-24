3e5c23cc1cdabe266d7000bea5e6a86cc74e2fa6
sejin.kim1904@gmail.com <sejin.kim1904@gmail.com>
2020-10-24 14:03:45 -0600

    feat(application): Add methods for login patterns
    - Add before_action callback to require login
    - Update routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,47 @@
 class ApplicationController < ActionController::API
+  before_action :require_login
+
   def encode_token(payload)
-    JWT.encode(payload, secret)
+    JWT.encode(payload, secret, algorithm)
   end
 
   def secret
     Rails.application.credentials.secret_key_base
+  end
+
+  def algorithm
+    'HS256'
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decode_token
+    token = auth_header.split(' ')[1] if auth_header
+    begin
+      JWT.decode(token, secret, true, algorithm: algorithm)
+    rescue JWT::DecodeError
+      []
+    end
+  end
+
+  def session_team
+    if decode_token.present?
+      team_id = decode_token[0]['team_id']
+      @team = Team.find(team_id)
+    else
+      nil
+    end
+  end
+
+  def logged_in?
+    !!session_team
+  end
+
+  def require_login
+    render json: {
+      message: 'Please Login'
+      }, status: :unauthorized unless logged_in?
   end
 end

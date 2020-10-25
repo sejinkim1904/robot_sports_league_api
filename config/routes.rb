250ef721a14493756840fa117c267b88ca7aa4b4
sejin.kim1904@gmail.com <sejin.kim1904@gmail.com>
2020-10-25 03:25:23 -0600

    feat(routes): Update namespacing
    - Update bots_controller
    - Add route for generate_roster

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :teams, only: [:create, :update] do
+      resources :teams, only: [:create, :update]
+
+      namespace :teams do
         resources :bots, only: [:create]
+        post '/generate_roster', to: 'rosters#generate_roster'
       end
+
       post '/login', to: 'auth#login'
       get '/auto_login', to: 'auth#auto_login'
     end

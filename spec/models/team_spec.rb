941ab2bbf5f65bd5cf718714234d51c214c6ded8
sejin.kim1904@gmail.com <sejin.kim1904@gmail.com>
2020-10-25 03:27:20 -0600

    test(roster): Create/update specs

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -25,5 +25,57 @@ describe Team do
       expect(team.bots.size).to eq(100)
       expect(team.bots.first).to be_instance_of(Bot)
     end
+
+    it '#select_bots' do
+      team.generate_bots
+
+      expect(team.select_bots.size).to eq(15)
+      expect(team.select_bots.first).to be_instance_of(Roster)
+      team.select_bots.each do |roster_bot|
+        expect(roster_bot.role).to eq('benchwarmer')
+      end
+    end
+
+    it '#generate_roster' do
+      team.generate_bots
+      roster = team.generate_roster
+
+      expect(roster.size).to eq(15)
+      expect(roster.first).to be_instance_of(Roster)
+
+      roster.sort_by(&:role)[0..4].each do |roster_bot|
+        expect(roster_bot.role).to eq('alternate')
+      end
+
+      roster.sort_by(&:role)[5..14].each do |roster_bot|
+        expect(roster_bot.role).to eq('starter')
+      end
+    end
+
+    it '#current_roster' do
+      team.generate_bots
+      team.generate_roster
+      roster = team.current_roster
+
+      expect(roster.size).to eq(15)
+      expect(roster.first).to be_instance_of(Roster)
+
+      roster[0..9].each do |roster_bot|
+        expect(roster_bot.role).to eq('starter')
+      end
+
+      roster[10..14].each do |roster_bot|
+        expect(roster_bot.role).to eq('alternate')
+      end
+    end
+
+    it '#delete_roster' do
+      team.generate_bots
+      team.generate_roster
+
+      expect(team.current_roster.size).to eq(15)
+      team.delete_roster
+      expect(team.current_roster.empty?).to be(true)
+    end
   end
 end

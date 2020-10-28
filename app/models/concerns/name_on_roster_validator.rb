4b7140a3e1d1e4ebeddb784805b1eb22bf37a143
sejin.kim1904@gmail.com <sejin.kim1904@gmail.com>
2020-10-27 19:22:58 -0600

    style(validators): Update condtionals

--- app/models/concerns/name_on_roster_validator.rb
+++ app/models/concerns/name_on_roster_validator.rb
@@ -1,7 +1,6 @@
 class NameOnRosterValidator < ActiveModel::Validator
   def validate(record)
-    return if record.benchwarmer?
-    return if record.in?(record.team.current_roster)
+    return if record.benchwarmer? || record.in?(record.team.current_roster)
     return unless record.bot.name.in?(record.team.current_roster.bot_names)
 
     record.errors.add :bot_name, 'must be unique on roster'
